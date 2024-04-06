b0d72d4730dd074c7a9f7bd875092db1ed180190
Kelly Sizer <kelly@sizerllc.com>
2024-04-05 20:19:29 -0500

    Ensuring exit code from py script is passed through sh wrapper

--- pattern.sh
+++ pattern.sh
@@ -9,5 +9,11 @@ source "$SCRIPT_DIR/venv/bin/activate"
 # Execute the Python script, passing along any command line arguments
 python "$SCRIPT_DIR/pattern.py" "$@"
 
+# Capture the exit code of the Python script
+PYTHON_EXIT_CODE=$?
+
 # Optionally, deactivate the virtual environment
 deactivate
+
+# Exit the shell script with the Python script's exit code
+exit $PYTHON_EXIT_CODE
